a20611b2ae9786758d756b99979133b4650befbf
Mostafa Hisham <mostafa@Mostafas-MacBook-Pro.local>
2020-09-21 19:41:24 +0200

    Switched to class based views

--- inovola_task/coffee/urls.py
+++ inovola_task/coffee/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("api/", views.apiOverview, name="api-overview"),
-    path("api/machine-detail/<str:pk1>/<str:pk2>/<str:pk3>/", views.machineDetail, name="machine-detail"),
-    path("api/machine-type/<str:pk>/", views.machineType, name="machine-type"),
-    path("api/machine-model/<str:pk>/", views.machineModel, name="machine-model"),
-    path("api/machine-water/<str:pk>/", views.machineWater, name="machine-water"),
-    path("api/pod-detail/<str:pk1>/<str:pk2>/<str:pk3>/", views.podDetail, name="pod-detail"),
-    path("api/pod-type/<str:pk>/", views.podType, name="pod-type"),
-    path("api/pod-flavor/<str:pk>/", views.podFlavor, name="pod-flavor"),
-    path("api/pod-size/<str:pk>/", views.podSize, name="pod-size"),
+    path("api/", views.ApiOverview.as_view(), name="api-overview"),
+    path("api/machine-detail/<str:pk1>-<str:pk2>-<str:pk3>/", views.MachineDetail.as_view(), name="machine-detail"),
+    path("api/machine-type/<str:pk>/", views.MachineType.as_view(), name="machine-type"),
+    path("api/machine-model/<str:pk>/", views.MachineModel.as_view(), name="machine-model"),
+    path("api/machine-water/<str:pk>/", views.MachineWater.as_view(), name="machine-water"),
+    path("api/pod-detail/<str:pk1>-<str:pk2>-<str:pk3>/", views.PodDetail.as_view(), name="pod-detail"),
+    path("api/pod-type/<str:pk>/", views.PodType.as_view(), name="pod-type"),
+    path("api/pod-flavor/<str:pk>/", views.PodFlavor.as_view(), name="pod-flavor"),
+    path("api/pod-size/<str:pk>/", views.PodSize.as_view(), name="pod-size"),
 ]
--- inovola_task/coffee/views.py
+++ inovola_task/coffee/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .serializers import MachineSerializer, PodSerializer
 from .models import Machine, Pod
 
@@ -10,80 +11,88 @@ def index(request):
 
 
 # Overview for the rest api
-@api_view(["GET"])
-def apiOverview(request):
-    api_urls = {
-        'Machine Detail': 'api/machine-detail/<str:pk1>/<str:pk2>/<str:pk3>/',
-        'Machine Type': 'api/machine-type/<str:pk>/',
-        'Machine Model': 'api/machine-model/<str:pk>/',
-        'Machine Water': 'api/machine-water/<str:pk>/',
-        'Pod Detail': 'api/pod-detail/<str:pk1>/<str:pk2>/<str:pk3>/',
-        'Pod Type': 'api/pod-type/<str:pk>/',
-        'Pod Flavor': 'api/pod-flavor/<str:pk>/',
-        'Pod Size': 'api/pod-size/<str:pk>/',
-    }
-    return Response(api_urls)
+class ApiOverview(APIView):
+
+    def get(self, request):
+        api_urls = {
+            'Machine Detail': 'api/machine-detail/<str:pk1>/<str:pk2>/<str:pk3>/',
+            'Machine Type': 'api/machine-type/<str:pk>/',
+            'Machine Model': 'api/machine-model/<str:pk>/',
+            'Machine Water': 'api/machine-water/<str:pk>/',
+            'Pod Detail': 'api/pod-detail/<str:pk1>/<str:pk2>/<str:pk3>/',
+            'Pod Type': 'api/pod-type/<str:pk>/',
+            'Pod Flavor': 'api/pod-flavor/<str:pk>/',
+            'Pod Size': 'api/pod-size/<str:pk>/',
+        }
+        return Response(api_urls)
 
 
 # Filter machines by all categories
-@api_view(['GET'])
-def machineDetail(request, pk1, pk2, pk3):
-    machines = Machine.objects.filter(types=pk1, model=pk2, is_water=pk3)
-    serializer = MachineSerializer(machines, many=True)
-    return Response(serializer.data)
+class MachineDetail(APIView):
+
+    def get(self, request, pk1, pk2, pk3):
+        machines = Machine.objects.filter(types=pk1, model=pk2, is_water=pk3)
+        serializer = MachineSerializer(machines, many=True)
+        return Response(serializer.data)
 
 
 # Filter machines by product type
-@api_view(['GET'])
-def machineType(request, pk):
-    machines = Machine.objects.filter(types=pk)
-    serializer = MachineSerializer(machines, many=True)
-    return Response(serializer.data)
+class MachineType(APIView):
+
+    def get(self, request, pk):
+        machines = Machine.objects.filter(types=pk)
+        serializer = MachineSerializer(machines, many=True)
+        return Response(serializer.data)
 
 
 # Filter machines by model
-@api_view(['GET'])
-def machineModel(request, pk):
-    machines = Machine.objects.filter(model=pk)
-    serializer = MachineSerializer(machines, many=True)
-    return Response(serializer.data)
+class MachineModel(APIView):
+
+    def get(self,request, pk):
+        machines = Machine.objects.filter(model=pk)
+        serializer = MachineSerializer(machines, many=True)
+        return Response(serializer.data)
 
 
 # Filter machines by water line compatibility
-@api_view(['GET'])
-def machineWater(request, pk):
-    machines = Machine.objects.filter(is_water=pk)
-    serializer = MachineSerializer(machines, many=True)
-    return Response(serializer.data)
+class MachineWater(APIView):
+
+    def get(self, request, pk):
+        machines = Machine.objects.filter(is_water=pk)
+        serializer = MachineSerializer(machines, many=True)
+        return Response(serializer.data)
 
 
 # Filter pods by all categories
-@api_view(['GET'])
-def podDetail(request, pk1, pk2, pk3):
-    pods = Pod.objects.filter(types=pk1, flavor=pk2, size=pk3)
-    serializer = PodSerializer(pods, many=True)
-    return Response(serializer.data)
+class PodDetail(APIView):
+
+    def get(self, request, pk1, pk2, pk3):
+        pods = Pod.objects.filter(types=pk1, flavor=pk2, size=pk3)
+        serializer = PodSerializer(pods, many=True)
+        return Response(serializer.data)
 
 
 # Filter pods by product type
-@api_view(['GET'])
-def podType(request, pk):
-    pods = Pod.objects.filter(types=pk)
-    serializer = PodSerializer(pods, many=True)
-    return Response(serializer.data)
+class PodType(APIView):
+
+    def get(self, request, pk):
+        pods = Pod.objects.filter(types=pk)
+        serializer = PodSerializer(pods, many=True)
+        return Response(serializer.data)
 
 
 # Filter pods by flavor
-@api_view(['GET'])
-def podFlavor(request, pk):
-    pods = Pod.objects.filter(flavor=pk)
-    serializer = PodSerializer(pods, many=True)
-    return Response(serializer.data)
+class PodFlavor(APIView):
+
+    def get(self, request, pk):
+        pods = Pod.objects.filter(flavor=pk)
+        serializer = PodSerializer(pods, many=True)
+        return Response(serializer.data)
 
 
 # Filter pods by size
-@api_view(['GET'])
-def podSize(request, pk):
-    pods = Pod.objects.filter(size=pk)
-    serializer = PodSerializer(pods, many=True)
-    return Response(serializer.data)
+class PodSize(APIView):
+    def get(self, request, pk):
+        pods = Pod.objects.filter(size=pk)
+        serializer = PodSerializer(pods, many=True)
+        return Response(serializer.data)
